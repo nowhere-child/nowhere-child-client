2c3bbbc1f70111b1309ab1703d0f5ef1cf32f66b
Sungyeon Jung <yeonnybus@gmail.com>
2025-04-30 09:56:14 +0900

    feat: 홈 페이지(코드 입력) 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,35 @@
-// src/pages/Home.tsx
-export default function Home() {
+import { CodeInputs } from "@/components/home/CodeInputs";
+import { FooterBar } from "@/components/home/FooterBar";
+import { StatusMessage } from "@/components/home/StatusMessage";
+import { useEnterCode } from "@/hooks/useEnterCode";
+import { useIOSScrollFix } from "@/hooks/useIOSScrollFix";
+
+const EnterCodePage = () => {
+  const hook = useEnterCode();
+  useIOSScrollFix(hook.isFocused);
+
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-bold">야외 방탈출</h1>
-      <a
-        className="rounded-lg bg-primary px-6 py-3 text-white hover:bg-primary/90"
-        href="/mission/1"
-      >
-        미션 시작
-      </a>
-    </main>
+    <div className="flex flex-col h-dvh bg-black">
+      {/* 배경 이미지 영역 */}
+      <div className="flex flex-col flex-1 bg-[url('../assets/bg-2x.png')] bg-cover bg-center">
+        <div className="flex flex-col items-center justify-between flex-1 px-4 pt-10">
+          <h1 className="text-3xl font-bold text-white mb-6">Night Skull</h1>
+
+          <div>
+            <StatusMessage done={hook.isComplete} />
+            <CodeInputs
+              codes={hook.codes}
+              refs={hook.inputRefs}
+              onChange={hook.setCodes}
+              onKey={hook.backspace}
+              setFocus={hook.setIsFocused}
+            />
+          </div>
+        </div>
+      </div>
+      {/* 버튼 영역 */}
+      <FooterBar disabled={!hook.isComplete} onSubmit={hook.submit} />
+    </div>
   );
-}
+};
+export default EnterCodePage;
