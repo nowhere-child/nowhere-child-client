9d4eeeb4ec284011fa2ed2183f5dc8e4859afedd
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-02 17:59:23 +0900

    feat: 공통컴포넌트화(bg, message prop 추가)

--- src/components/home/FooterBar.tsx
+++ src/components/home/FooterBar.tsx
@@ -2,17 +2,21 @@ import { Button } from "@/components/ui/button";
 export const FooterBar = ({
   disabled,
   onSubmit,
+  bg = "#000000",
+  message,
 }: {
   disabled: boolean;
   onSubmit: () => void;
+  bg?: string;
+  message: string;
 }) => (
-  <div className="bg-black flex items-center justify-center py-4">
+  <div className={`bg-[${bg}] flex items-center justify-center py-4`}>
     <Button
       onClick={onSubmit}
       disabled={disabled}
-      className={`w-full max-w-xs h-12 text-lg rounded-[12px] bg-[#F2F2F2] text-[#191919] disabled:bg-[#313131] disabled:text-[#565656]`}
+      className={`w-full max-w-xs h-12 text-lg rounded-[12px] bg-[#3182F6] text-[#FCFCFC] disabled:bg-[#313131] disabled:text-[#565656]`}
     >
-      입장하기
+      {message}
     </Button>
   </div>
 );
