062e5dd7ba5c376e56fbc589ac6af4a12f834116
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-05 21:51:47 +0900

    fix: 가로스크롤 방지

--- src/components/mission/MissionRenderer.tsx
+++ src/components/mission/MissionRenderer.tsx
@@ -7,14 +7,14 @@ export default function MissionRenderer({ step }: { step: Step }) {
   const goNext = useMissionStore((s) => s.goNext);
 
   return (
-    <section className="h-dvh flex flex-col bg-[#1A1A1A] px-5">
+    <section className="h-dvh flex flex-col overflow-x-hidden bg-[#1A1A1A] px-5">
       {/* 스냅 컨테이너 */}
       <h2 className="text-xl">{step.title}</h2>
-      <div className="flex-1 overflow-y-scroll snap-y snap-mandatory scroll-smooth space-y-4 justify-center w-full items-center">
+      <div className="flex-1 overflow-y-scroll snap-y snap-mandatory scroll-smooth space-y-4 justify-center items-center">
         {step.blocks.map((block, i) => (
           <div
             key={i}
-            className="snap-start min-h-full flex items-center flex-col justify-center gap-5 mb-[66px]"
+            className="snap-start overflow-x-hidden min-h-full flex items-center flex-col justify-center gap-5 mb-[66px]"
           >
             <BlockRenderer block={block} />
             {i === step.blocks.length - 1 && (
