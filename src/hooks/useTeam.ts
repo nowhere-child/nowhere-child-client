4c0a12ab95f67fa0161852c68794b49120aa4965
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-12 23:39:17 +0900

    fix: useCheckTeamName mutation으로 변경

--- src/hooks/useTeam.ts
+++ src/hooks/useTeam.ts
@@ -1,13 +1,12 @@
 // src/hooks/useTeam.ts
 import * as TeamAPI from "@/api/team";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 
 /** 팀 이름 중복 확인 */
 export const useCheckTeamName = (teamName: string) => {
-  return useQuery({
-    queryKey: ["checkTeamName", teamName],
-    queryFn: () => TeamAPI.checkTeamName(teamName),
-    enabled: !!teamName, // 빈 문자열이면 호출 방지
+  console.log("팀명 중복 확인", teamName);
+  return useMutation({
+    mutationFn: () => TeamAPI.checkTeamName(teamName),
   });
 };
 
