11bab81b3b497b54ed29ba2e075e1eb38161c2f2
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-16 20:15:16 +0900

    feat: Image, Text 블록 렌더링

--- src/components/mission/BlockRenderer.tsx
+++ src/components/mission/BlockRenderer.tsx
@@ -1,5 +1,7 @@
 import { FolderNote } from "./blocks/FolderNote";
+import ImageBlock from "./blocks/ImageBlock";
 import { StickyNote } from "./blocks/StickyNote";
+import TextBlock from "./blocks/TextBlock";
 import { TextNote } from "./blocks/TextNote";
 import { MissionBlock } from "./MissionRenderer";
 
@@ -13,6 +15,12 @@ export default function BlockRenderer({ block }: { block: MissionBlock }) {
       );
     case "Sticky":
       return <StickyNote text={block.text} />;
+
+    case "IMAGE":
+      return <ImageBlock text={block.text} />;
+
+    case "TEXT":
+      return <TextBlock text={block.text} />;
 
     default:
       return <TextNote text={block.text} />;
