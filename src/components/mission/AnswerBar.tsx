27582cfd5337952030cac509403e4cb72534756b
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-16 20:14:37 +0900

    feat: 미션 Post 붙이기

--- src/components/mission/AnswerBar.tsx
+++ src/components/mission/AnswerBar.tsx
@@ -1,21 +1,58 @@
 // src/components/mission/AnswerBar.tsx
+import { submitAnswer } from "@/api/answer";
+import { useMissionStore } from "@/store/missionStore";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import AnswerInput from "./answer/AnswerInput";
+import GPS from "./answer/GPS";
+import SingleButton from "./answer/SingleButton";
 
 export interface AnswerData {
   gameId: number;
   order: number;
   answerBlockType: string;
   language: string;
+  answer?: string;
 }
 interface Props {
   config: AnswerData;
-  onSuccess: () => void;
 }
 
-export default function AnswerBar({ config, onSuccess }: Props) {
+export default function AnswerBar({ config }: Props) {
   const [v, setV] = useState("");
-  console.log("AnswerBar config", config);
+
+  /* ① 성공 기록 업로드 */
+  const { mutateAsync: finishMission } = useMutation({
+    mutationFn: () =>
+      submitAnswer({
+        memberId: 1,
+        gameId: 1,
+        missionOrder: missionId!,
+        language: "KO",
+        answer: v,
+      }),
+  });
+
+  const { missionId, setMissionId } = useMissionStore();
+  const qc = useQueryClient();
+
+  /* ② 정답 성공 시 */
+  const handleNext = async () => {
+    const res = await finishMission(); // AnswerBar로 넘김
+    setV("");
+    console.log("res", res);
+
+    const next = (missionId ?? 0) + 1;
+    setMissionId(next); // store 갱신 → MissionEngine re-fetch
+    qc.invalidateQueries({
+      // 캐시 갱신
+      queryKey: ["mission", next],
+    });
+    qc.invalidateQueries({
+      queryKey: ["answer", next],
+    });
+    window.scrollTo(0, 0);
+  };
 
   switch (config.answerBlockType) {
     case "INPUT_STRING":
@@ -23,7 +60,7 @@ export default function AnswerBar({ config, onSuccess }: Props) {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            onSuccess();
+            handleNext();
           }}
           className="px-5 pb-6 safe-bottom"
         >
@@ -36,25 +73,11 @@ export default function AnswerBar({ config, onSuccess }: Props) {
       );
 
     case "SINGLE_BUTTON":
-      return (
-        <div className="px-5 pb-6 safe-bottom">
-          <button
-            onClick={onSuccess}
-            className="w-full py-4 rounded-[20px] bg-primary text-white text-lg"
-          >
-            {"확인"}
-          </button>
-        </div>
-      );
+      return <SingleButton handleNext={handleNext} setV={setV} />;
     case "GPS":
       return (
         <div className="px-5 pb-6 safe-bottom">
-          <button
-            onClick={onSuccess}
-            className="w-full py-4 rounded-[20px] bg-primary text-white text-lg"
-          >
-            {"확인"}
-          </button>
+          <GPS onSuccess={handleNext} config={config} setV={setV} />
         </div>
       );
 
