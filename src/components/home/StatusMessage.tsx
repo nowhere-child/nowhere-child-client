508f23aa33bc3f71f9a567494d7b8de45093c641
Sungyeon Jung <yeonnybus@gmail.com>
2025-05-18 02:33:57 +0900

    feat: 미션 프로그레스 바, 홈 수정

--- src/components/home/StatusMessage.tsx
+++ src/components/home/StatusMessage.tsx
@@ -3,10 +3,10 @@ export const StatusMessage = ({ done }: { done: boolean }) => (
   <div className="h-6 mb-[14px] flex w-full justify-center text-white">
     {done ? (
       <div className="flex items-center gap-1">
-        입장코드 확인 완료 <CircleCheck size={20} className="text-blue-500" />
+        입장코드 입력 완료 <CircleCheck size={20} className="text-blue-500" />
       </div>
     ) : (
-      <span className="text-gray-200">입장코드를 입력해주세요.</span>
+      <span className="text-gray-200">입장코드 6자리를 입력해주세요.</span>
     )}
   </div>
 );
